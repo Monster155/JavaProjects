965f4d551b025d72c662ab3e1e929138c7d584f3
Alexandr <alexandr-d23@mail.ru>
2019-11-14 00:28:39 +0300

    F

---
+++ src/Insertion.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Insertion {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the quantity of array");
+        int quan=Integer.parseInt(sc.nextLine());
+        int[] array=new int[quan];
+        for(int i=0;i<array.length;i++){
+            array[i]=(int)(Math.random()*100);
+        }
+        insertion(array);
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+    }
+    public static void insertion(int[] array){
+        int j;
+        for(int i=1;i<array.length;i++){
+            j=i;
+            while(j>0&&array[j]<array[j-1]){
+                array[j]+=array[j-1];
+                array[j-1]=array[j]-array[j-1];
+                array[j]-=array[j-1];
+                j--;
+            }
+        }
+    }
+}
